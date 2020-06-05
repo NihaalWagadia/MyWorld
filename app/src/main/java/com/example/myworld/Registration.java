a74ae29fb573fceddecec4b7658d65ac954dff87
NihaalWagadia <nwagadia@gmail.com>
2020-06-04 23:29:55 -0400

    FireStore

--- app/src/main/java/com/example/myworld/Registration.java
+++ app/src/main/java/com/example/myworld/Registration.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,19 +14,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Registration extends AppCompatActivity {
 
-
+    public static final String TAG = "Registration";
     EditText userName, password, email;
     FirebaseAuth firebaseAuth;
     Button registerButton;
     TextView loginText;
+    FirebaseFirestore firebaseFirestore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,7 @@ public class Registration extends AppCompatActivity {
         loginText = findViewById(R.id.go_to_login_text);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         //already logged in/existing user
 
@@ -51,8 +60,9 @@ public class Registration extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String emailId = email.getText().toString().trim();
+                final String emailId = email.getText().toString().trim();
                 String passwordCheck = password.getText().toString().trim();
+                final String uName = userName.getText().toString();
 
                 if(TextUtils.isEmpty(emailId)){
                     email.setError("Email is required");
@@ -71,6 +81,18 @@ public class Registration extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Registration.this, "User Created", Toast.LENGTH_SHORT).show();
+                            //creating/referencing to that field/document
+                            userId = firebaseAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = firebaseFirestore.collection("People").document(userId);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Name", uName);
+                            user.put("Email", emailId);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "User Created"+userId);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MapsActivity.class));
                         }
                         else {
