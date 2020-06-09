accf251e035e209fe9567cbdbf571a9b385ea8b0
NihaalWagadia <nwagadia@gmail.com>
2020-06-09 02:45:41 -0400

    statusBar

--- app/src/main/java/com/example/myworld/Profile.java
+++ app/src/main/java/com/example/myworld/Profile.java
@@ -7,22 +7,37 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class Profile extends AppCompatActivity {
 
     EditText userName, userEmail, userPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.content_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         userName = findViewById(R.id.edit_username);
         userEmail = findViewById(R.id.edit_email);
         userPassword = findViewById(R.id.edit_password);
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
 
 
     }
