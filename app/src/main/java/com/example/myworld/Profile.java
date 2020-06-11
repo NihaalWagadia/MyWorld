84d5d85895a7395ad07b5c7c7950cde8a5b81aad
NihaalWagadia <nwagadia@gmail.com>
2020-06-11 01:25:49 -0400

    ProfileUpdate

--- app/src/main/java/com/example/myworld/Profile.java
+++ app/src/main/java/com/example/myworld/Profile.java
@@ -1,46 +1,88 @@
 package com.example.myworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    EditText userName, userEmail, userPassword;
+    EditText userName, userEmail;
+    Button confirmButton;
     FirebaseFirestore firebaseFirestore;
     DocumentReference documentReference;
     FirebaseAuth firebaseAuth;
     String userId;
+    FirebaseUser currentUser;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    TextView uName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_profile);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout_profile);
+        //navigationView = findViewById(R.id.nav_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_profile);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        uName = header.findViewById(R.id.username_header);
+
         userName = findViewById(R.id.edit_username);
         userEmail = findViewById(R.id.edit_email);
-        userPassword = findViewById(R.id.edit_password);
+        confirmButton = findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateProfile();
+            }
+        });
         Window window = this.getWindow();
 
         // clear FLAG_TRANSLUCENT_STATUS flag:
@@ -54,6 +96,8 @@ public class Profile extends AppCompatActivity {
 
 
         firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+
         firebaseFirestore = FirebaseFirestore.getInstance();
         userId = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
         documentReference = firebaseFirestore.collection("People").document(userId);
@@ -62,11 +106,79 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 assert documentSnapshot != null;
-                userName.setText(documentSnapshot.getString("Name"));
-                userEmail.setText(documentSnapshot.getString("Email"));
-
+                if (documentSnapshot != null) {
+                    userName.setText(documentSnapshot.getString("Name"));
+                    userEmail.setText(documentSnapshot.getString("Email"));
+                    uName.setText(documentSnapshot.getString("Name"));
+                }
             }
         });
     }
 
+    private void updateProfile() {
+
+        if (userName.getText().toString().isEmpty() || userEmail.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Empty field", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        String userEmailEdit = userEmail.getText().toString();
+        currentUser.updateEmail(userEmailEdit).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                documentReference = firebaseFirestore.collection("People").document(userId);
+                Map<String, Object> editedUser = new HashMap<>();
+                editedUser.put("Name", userName.getText().toString());
+                editedUser.put("Email", userEmail.getText().toString());
+                uName.setText(userName.getText().toString());
+                documentReference.update(editedUser).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Profile Updated", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_about) {
+//            Intent intent = new Intent(this, Profile.class);
+//            startActivity(intent);
+        } else if (id == R.id.nav_upload) {
+//            Intent intent = new Intent(this, Profile.class);
+//            startActivity(intent);
+        } else if (id == R.id.nav_help) {
+//            Intent intent = new Intent(this, Profile.class);
+//            startActivity(intent);
+        } else if (id == R.id.nav_logout) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                firebaseAuth.signOut();
+                finish();
+                Intent myIntent = new Intent(getApplicationContext(), Login.class);
+                startActivity(myIntent);
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
