135a49ad3a47bbc81bd0934bfd48be67ce33bba3
NihaalWagadia <nwagadia@gmail.com>
2020-06-24 23:22:32 -0400

    HybridMap&InternetCheck

---
+++ app/src/main/java/com/example/myworld/ManualChecker.java
@@ -0,0 +1,21 @@
+package com.example.myworld;
+
+import android.app.Application;
+
+public class ManualChecker extends Application {
+    private static ManualChecker manualChecker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        manualChecker= this;
+    }
+
+    public static synchronized ManualChecker getInstance(){
+        return manualChecker;
+    }
+
+    public void setConnectivityListener(ConnectionReceiver.ConnectivityReceiverListener listener){
+        ConnectionReceiver.connectivityReceiverListener = listener;
+    }
+}
--- app/src/main/java/com/example/myworld/MapsActivity.java
+++ app/src/main/java/com/example/myworld/MapsActivity.java
@@ -9,6 +9,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -24,11 +27,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -40,16 +45,20 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
+import id.zelory.compressor.Compressor;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener, ConnectionReceiver.ConnectivityReceiverListener {
 
     private GoogleMap mMap;
     DrawerLayout drawerLayout;
-    NavigationView navigationView;
     TextView uName;
     FirebaseFirestore firebaseFirestore;
     FirebaseAuth firebaseAuth;
@@ -57,28 +66,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     CollectionReference allLocationRef;
     String userId;
     String name;
-    FirebaseUser user;
-    ArrayList<UserLocation>userLocations = new ArrayList<>();
+    ArrayList<UserLocation> userLocations = new ArrayList<>();
+    Map<LatLng, String> markerDetailsName = new HashMap<>();
+    Map<LatLng, String> markerDetailsImage = new HashMap<>();
+    private ClusterManager<MyItem> clusterManager;
+    //private List<MyItem>items = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         drawerLayout = findViewById(R.id.drawer_layout);
         firebaseFirestore = FirebaseFirestore.getInstance();
-        Intent myIntent = getIntent();
-        if (myIntent!= null){
-//            latLngs = myIntent.getParcelableArrayListExtra("collection");
-        }
-//        Log.d("ohhh",String.valueOf(latLngs));
         allLocationRef = firebaseFirestore.collection("Universal Data");
+        //check connection manually
+        checkInternetConnection();
 
-//        readData(new FireStoreCallback() {
-//            @Override
-//            public void onCallback(List<LatLng> latLngList) {
-//                Log.d("321", String.valueOf(latLngs));
-//            }
-//        });
+
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -114,10 +119,55 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-//        Log.d("outside", String.valueOf(latLngs));
-        for(UserLocation lo : userLocations){
+        for (UserLocation lo : userLocations) {
             Log.d("abcd", String.valueOf(lo.getLati()));
         }
+    }
+
+    private void checkInternetConnection() {
+        boolean isConnected = ConnectionReceiver.isConnected();
+        showSnackBar(isConnected);
+
+        if(!isConnected){
+            changeActivity();
+        }
+    }
+
+    private void changeActivity() {
+
+            Intent intent = new Intent(this, Offline.class);
+            startActivity(intent);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+
+        ConnectionReceiver connectionReceiver = new ConnectionReceiver();
+        registerReceiver(connectionReceiver, intentFilter);
+        //register connection status listener
+        ManualChecker.getInstance().setConnectivityListener(this);
+    }
+
+    private void showSnackBar(boolean isConnected) {
+        String message;
+        int color;
+        if(isConnected){
+            message = "You online";
+            color = Color.WHITE;
+        }
+        else{
+            message = "check internet";
+            color = Color.RED;
+        }
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.drawer_layout), message, Snackbar.LENGTH_LONG);
+        View view = snackbar.getView();
+        TextView textView = view.findViewById(com.google.android.material.R.id.snackbar_text);
+        textView.setTextColor(color);
+        snackbar.show();
     }
 
     private void getUserLocation() {
@@ -125,126 +175,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         allLocationRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                if(queryDocumentSnapshots !=null){
+                if (queryDocumentSnapshots != null) {
                     userLocations = new ArrayList<>();
-                    for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+
                         UserLocation userLocation = documentSnapshot.toObject(UserLocation.class);
                         userLocations.add(userLocation);
                         Log.d("abcd", String.valueOf(userLocation.getLati()));
                         LatLng latLng = new LatLng(userLocation.getLati(), userLocation.getLongi());
-                        mMap.addMarker(new MarkerOptions().position(latLng).title("Location"));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        // customInfoWindowAdapter = new CustomInfoWindowAdapter(getApplicationContext(), userLocation.getUserName(), userLocation.getImageId());
+
+                        markerDetailsName.put(latLng, userLocation.getUserName());
+                        markerDetailsImage.put(latLng, userLocation.getImageId());
+                        //mMap.addMarker(new MarkerOptions().position(latLng));
+//                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//                        items.add(new MyItem(latLng));
+//                        clusterManager.addItem(items);
+                        MyItem offsetItem = new MyItem(latLng);
+                        clusterManager.addItem(offsetItem);
+                        clusterManager.cluster();
 
                     }
                 }
+
 
             }
         });
 
     }
-
-//    private void readData(final FireStoreCallback fire){
-//        allLocationRef.get()
-//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
-//                            double lati = documentSnapshot.getDouble("Latitude");
-//                            double lngi = documentSnapshot.getDouble("Longitude");
-//                            LatLng latLng = new LatLng(lati, lngi);
-//                            latLngs.add(latLng);
-//
-//                        }
-//                        fire.onCallback(latLngs);
-//                    }
-//                });
-//    }
-
-    private interface FireStoreCallback{
-        void onCallback(List<LatLng> latLngList);
-    }
-
-    private void allUsersLocation() {
-
-
-//        allLocationRef.get()
-//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
-//                            double lati = documentSnapshot.getDouble("Latitude");
-//                            double lngi = documentSnapshot.getDouble("Longitude");
-//                            LatLng latLng = new LatLng(lati, lngi);
-//                            latLngs.add(latLng);
-//                            Log.d("123", String.valueOf(latLng));
-//
-//                        }
-//                    }
-//                });
-
-//                firebaseFirestore1.collection("Universal Data").get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if(task.isSuccessful()){
-//                            //latLngs = new ArrayList<>();
-//                            for(QueryDocumentSnapshot queryDocumentSnapshot : Objects.requireNonNull(task.getResult())){
-//                                Log.d("MapsActivity", queryDocumentSnapshot.getId() +
-//                                        "===" + queryDocumentSnapshot.getData());
-//                               // Log.d("material2", String.valueOf(latLngs.size()));
-//                                double lati = queryDocumentSnapshot.getDouble("Latitude");
-//                                double lngi = queryDocumentSnapshot.getDouble("Longitude");
-//                                LatLng latLng = new LatLng(lati, lngi);
-//                                latLngs.add(latLng);
-////                                DocumentReference documentReference1 = firebaseFirestore.collection("Universal Data").document(queryDocumentSnapshot.getId());
-//
-//
-////                                documentReference1.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-////                                    @Override
-////                                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-////                                        //assert documentSnapshot != null;
-////                                        if (documentSnapshot != null) {
-////                                            double lati = documentSnapshot.getDouble("Latitude");
-////                                            double lngi = documentSnapshot.getDouble("Longitude");
-////                                            LatLng latLng = new LatLng(lati, lngi);
-////                                            latLngs.add(latLng);
-////                                            Log.d("Design", String.valueOf(latLngs));
-////                                        }
-////
-////                                    }
-////                                });
-//
-//                               // getLatLng(queryDocumentSnapshot.getId());
-//                                Log.d("Design", String.valueOf(latLngs));
-//
-//                            }
-//                        }
-//                        else{
-//                            Log.d("MapsActivity", "Error:"+ task.getException());
-//                        }
-//
-//                    }
-//                });
-    }
-
-//    private void getLatLng(String id) {
-//        DocumentReference documentReference = firebaseFirestore.collection("Universal Data").document(id);
-//
-//        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-//                //assert documentSnapshot != null;
-//                if (documentSnapshot != null) {
-//                    double lati = documentSnapshot.getDouble("Latitude");
-//                    double lngi = documentSnapshot.getDouble("Longitude");
-//                    LatLng latLng = new LatLng(lati, lngi);
-//                    latLngs.add(latLng);
-//                    Log.d("Design", String.valueOf(latLngs));
-//                }
-//
-//            }
-//        });
-//    }
 
     @Override
     public void onBackPressed() {
@@ -269,16 +227,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMinZoomPreference(2.0f); // Set a preference for minimum zoom (Zoom out).
+        mMap.setMaxZoomPreference(16.0f);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446),1));
+        clusterManager = new ClusterManager<>(this, mMap);
+        mMap.setOnCameraIdleListener(clusterManager);
+        mMap.setOnMarkerClickListener(clusterManager);
         getUserLocation();
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String userNames = markerDetailsName.get(marker.getPosition());
+                String imageUrl = markerDetailsImage.get(marker.getPosition());
+                mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(getApplicationContext(), userNames, imageUrl));
+                return false;
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//        for(int i=0; i<latLngs.size(); i++){
-//            mMap.addMarker(new MarkerOptions().position(latLngs.get(i)).title("Location"));
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLngs.get(i)));
-//        }
+            }
+        });
 
     }
 
@@ -289,21 +255,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (id == R.id.nav_profile) {
 
             Intent intent = new Intent(this, Profile.class);
-//            intent.putParcelableArrayListExtra("collection", latLngs);
             startActivity(intent);
 
         } else if (id == R.id.nav_about) {
-//            Intent intent = new Intent(this, Profile.class);
-//            startActivity(intent);
+
 
         } else if (id == R.id.nav_upload) {
             Intent intent = new Intent(this, Upload.class);
-//            intent.putParcelableArrayListExtra("collection", latLngs);
             startActivity(intent);
 
         } else if (id == R.id.nav_help) {
-//            Intent intent = new Intent(this, Profile.class);
-//            startActivity(intent);
+
 
         } else if (id == R.id.nav_logout) {
             FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -311,7 +273,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 firebaseAuth.signOut();
                 finish();
                 Intent myIntent = new Intent(getApplicationContext(), Login.class);
-//                myIntent.putParcelableArrayListExtra("collection", latLngs);
                 startActivity(myIntent);
             }
         }
@@ -320,4 +281,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        if(!isConnected){
+            changeActivity();
+        }
+        showSnackBar(isConnected);
+
+    }
 }
